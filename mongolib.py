bb5c275f3435577233129454cc455b20004ac279
dbm <dbm@dbm-vubu.(none)>
2014-04-14 13:50:31 -0700

    LICENSE

--- mongolib.py
+++ mongolib.py
@@ -1,17 +1,34 @@
-#
-# mongolib is a simple layer of convenience routines to help with everyday tasks using mongodb in python.
-#
-# The underlying principle is to provide the most pythonic, simplified, easy-to-write, easy-to-read syntax possible.
-#
-# examples:
-#    PyMongo> db.test.update({'foo': 'bar'}, {'$set': {'foo': 'baz'}})
-# becomes:
-#    mongolib> update(db.test, "foo==bar", foo='baz')
-#
-#    PyMongo> db.test.find({'foo': 'bar'}, fields={'foo':True, 'ban':True})
-# becomes:
-#    mongolib> query(db.test, "foo==bar", fields=('foo', 'ban'))
-#
+"""
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+    documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+    the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+    and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+    of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+    DEALINGS IN THE SOFTWARE.
+
+mongolib is a simple layer of convenience routines to help with everyday tasks using mongodb in python.
+
+The underlying principle is to provide the most pythonic, simplified, easy-to-write, easy-to-read syntax possible.
+
+examples:
+   PyMongo> db.test.update({'foo': 'bar'}, {'$set': {'foo': 'baz'}})
+becomes:
+   mongolib> update(db.test, "foo==bar", foo='baz')
+
+   PyMongo> db.test.find({'foo': 'bar'}, fields={'foo':True, 'ban':True})
+becomes:
+   mongolib> query(db.test, "foo==bar", fields=('foo', 'ban'))
+
+"""
+
 import pymongo, traceback
 
 def connect(host, port, db, user=None, pw=None):
